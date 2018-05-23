401c5ff1874dd0177305bf3dc2a25733710f947d
Sarang Shinde <in-svsarang@in-svsarang.local>
2018-05-23 12:11:11 +0530

    Story 9 : Sarang/Charu : Bating Table Component Created

---
+++ src/scoredetails/BatingTable.jsx
@@ -0,0 +1,33 @@
+import React from 'react';
+import { Container, Row, Col } from 'reactstrap';
+import { Table } from 'reactstrap';
+
+
+const BattingTable = () =>
+    <div>
+        <br />
+        <b>Batting Table</b>
+        <Table size="sm" responsive>
+            <thead bgcolor='#D3D3D3'>
+                <tr>
+                    <th>Batsman</th>
+                    <th>Runs</th>
+                    <th>Balls</th>
+                    <th>Fours</th>
+                    <th>Sixes</th>
+                    <th>Strike Rate</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                   
+                </tr>
+                <tr>
+                  
+                </tr>
+
+            </tbody>
+        </Table>
+    </div>
+
+export default BattingTable;
--- src/scoredetails/ScoreDetails.jsx
+++ src/scoredetails/ScoreDetails.jsx
@@ -1,15 +1,14 @@
 import React from 'react';
 import {Container,Row,Col} from 'reactstrap';
+import BattingTable from './BatingTable';
 
 
 const ScoreDetails = () => 
         <Container>
              <br/>
                 <Row>
-                    <Col lg={{size:6,offset:3}} md= {{size:6,offset:3}} sm={{size:12}} xs="12">
-                    <Row>
-                        ScoreDetails
-                    </Row>
+                    <Col lg={{size:6,offset:3}} md= {{size:6,offset:3}} >
+                        <BattingTable/>
                     </Col>
                 </Row>
         </Container>
